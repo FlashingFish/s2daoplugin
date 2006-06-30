25255f6c376d48c9a6dd96ea82bb84661b078e88
ichikawa <ichikawa@af1156fa-8a52-4dcd-968b-aa5184cf50cc>
2006-06-30 09:22:30 +0000

    AutoRegisterで登録されるコンポーネントを、diconファイルのあるパッケージの出力フォルダと同じ出力フォルダを持つパッケージに限定した
    git-svn-id: https://www.seasar.org/svn/sandbox/s2daoplugin/branches/org.seasar.s2daoplugin/TRY-060612@81 af1156fa-8a52-4dcd-968b-aa5184cf50cc

--- src/org/seasar/s2daoplugin/cache/deployment/deployer/FileSystemComponentAutoRegisterDeployer.java
+++ src/org/seasar/s2daoplugin/cache/deployment/deployer/FileSystemComponentAutoRegisterDeployer.java
@@ -16,17 +16,25 @@
 package org.seasar.s2daoplugin.cache.deployment.deployer;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceVisitor;
+import org.eclipse.core.resources.IStorage;
 import org.eclipse.core.runtime.CoreException;
+import org.eclipse.core.runtime.IPath;
+import org.eclipse.jdt.core.IJavaElement;
 import org.eclipse.jdt.core.IJavaProject;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
 import org.eclipse.jdt.core.IType;
 import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.JavaModelException;
 import org.seasar.kijimuna.core.dicon.model.IComponentElement;
+import org.seasar.s2daoplugin.S2DaoPlugin;
 import org.seasar.s2daoplugin.cache.deployment.IDeploymentContainer;
-import org.seasar.s2daoplugin.util.JavaProjectUtil;
 import org.seasar.s2daoplugin.util.JavaUtil;
 
 public class FileSystemComponentAutoRegisterDeployer extends AbstractAutoRegisterDeployer {
@@ -41,11 +49,19 @@ public class FileSystemComponentAutoRegisterDeployer extends AbstractAutoRegiste
 	}
 	
 	public void deploy() {
-		IJavaProject project = JavaCore.create(getAutoRegister().getProject());
-		for (int i = 0; i < getClassPatternSize(); i++) {
-			ClassPattern cp = getClassPattern(i);
-			JavaProjectUtil.visitSourceFolders(
-					project, cp.getPackageName(), new ProcessVisitor(cp));
+		IPackageFragmentRoot[] packages = findPackageFragmentRootSharedOutputLocation(
+				getAutoRegister().getStorage());
+		for (int i = 0; i < packages.length; i++) {
+			for (int j = 0; j < getClassPatternSize(); j++) {
+				ClassPattern cp = getClassPattern(j);
+				IPackageFragment base = packages[i].getPackageFragment(cp.getPackageName());
+				try {
+					base.getResource().accept(new ProcessVisitor(cp),
+							IResource.DEPTH_INFINITE, false);
+				} catch (CoreException e) {
+					S2DaoPlugin.log(e);
+				}
+			}
 		}
 		deploy(getAutoRegister());
 	}
@@ -73,6 +89,52 @@ public class FileSystemComponentAutoRegisterDeployer extends AbstractAutoRegiste
 		attribues.put(DICON_ATTR_CLASS, fqcn);
 		attribues.put(DICON_ATTR_AUTOBINDING, "auto");
 		return attribues;
+	}
+
+	private IPackageFragmentRoot findPackageFragmentRoot(IResource dicon) {
+		IJavaElement element = JavaCore.create(dicon.getParent());
+		if (element instanceof IPackageFragmentRoot) {
+			return (IPackageFragmentRoot) element;
+		}
+		while (element instanceof IPackageFragment) {
+			element = ((IPackageFragment) element).getParent();
+			if (element instanceof IPackageFragmentRoot) {
+				return (IPackageFragmentRoot) element;
+			}
+		}
+		return null;
+	}
+	
+	private IPackageFragmentRoot[] findPackageFragmentRootSharedOutputLocation(
+			IStorage storage) {
+		if (!(storage instanceof IResource)) {
+			return new IPackageFragmentRoot[0];
+		}
+		IPackageFragmentRoot root = findPackageFragmentRoot((IResource) storage);
+		IJavaProject project = root.getJavaProject();
+		Set result = new HashSet();
+		try {
+			IPath output1 = root.getRawClasspathEntry().getOutputLocation();
+			if (output1 == null) {
+				output1 = project.getOutputLocation();
+			}
+			IPackageFragmentRoot[] roots = project.getAllPackageFragmentRoots();
+			for (int i = 0; i < roots.length; i++) {
+				if (roots[i].getKind() == IPackageFragmentRoot.K_BINARY) {
+					continue;
+				}
+				IPath output2 = roots[i].getRawClasspathEntry().getOutputLocation();
+				if (output2 == null) {
+					output2 = project.getOutputLocation();
+				}
+				if (output1.equals(output2)) {
+					result.add(roots[i]);
+				}
+			}
+		} catch (JavaModelException e) {
+			S2DaoPlugin.log(e);
+		}
+		return (IPackageFragmentRoot[]) result.toArray(new IPackageFragmentRoot[result.size()]);
 	}
 	
 	
